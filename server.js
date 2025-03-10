083d72f45ec1d4c467fa26c23972ccce854f4e9a
kat0409 <kathiana119@gmail.com>
2025-03-09 19:00:32 -0500

    Updated server
    Server is now running on a different port (3000)

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const http = require('http')
 
 const server = http.createServer((request, response) => {
-
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html') //header is where we put the content type, authorization tokens, etc.; second parameter is the content type
+    response.write('<h1> Hello World </h1>') //what we send back to the client through the server
+    response.end()
 })
 
-const PORT  = process.env.PORT || 5000 //check if there is an environment variable
+const PORT  = process.env.PORT || 3000 //check if there is an environment variable
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
