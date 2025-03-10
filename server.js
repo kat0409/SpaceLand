12901589a2879ffc3332db7eaf439cd527fb8296
kat0409 <kathiana119@gmail.com>
2025-03-09 19:54:46 -0500

    Created a connection to the MySQL database
    Created a connection to the database

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
-const http = require('http')
+const http = require('http');
+const mysql = require('mysql2');
 
-const server = http.createServer((request, response) => {
-    response.statusCode = 200
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'space_land2025',
+    password: '$paceland25',
+    database: 'space_land_25'
+});
+
+connection.connect((err) => {
+    if(err){
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database!');
+})
+
+/*const server = http.createServer((request, response) => {
+    /*response.statusCode = 200
     response.setHeader('Content-Type', 'text/html') //header is where we put the content type, authorization tokens, etc.; second parameter is the content type
     response.write('<h1> Hello World </h1>') //what we send back to the client through the server
     response.end()
-})
+    
+})*/
+
+
 
 const PORT  = process.env.PORT || 3000 //check if there is an environment variable
 
