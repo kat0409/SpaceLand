a7690ae9d02de4318f4ff3fb703b4daa7600f8f0
kat0409 <kathiana119@gmail.com>
2025-03-11 14:27:20 -0500

    Updated routes.js
    Made conditionals to handle requests for getting all rides and all employees

--- Backend/routes.js
+++ Backend/routes.js
@@ -1,2 +1,14 @@
-const pool = require('./db'); 
-const queries = require('./queries');
+const actions = require('./actions'); 
+const {URL} = require('url');
+
+function routes(req, res) {
+    const url =  req.url;
+    const method = res.method;
+
+    if(url === '/rides' && method === 'GET'){
+        actions.getRides(req,res);
+    }
+    else if(url === '/employees' && method === 'GET'){
+        actions.getEmployees(req,res);
+    }
+};
