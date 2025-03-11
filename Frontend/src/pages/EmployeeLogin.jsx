e5df9cb65544054852ed813be858c86f4f39ccf1
Markpelico <mapelico@cougarnet.uh.edu>
2025-03-11 16:41:21 -0500

    Added the Employee page for certain view

---
+++ Frontend/src/pages/EmployeeLogin.jsx
@@ -0,0 +1,8 @@
+// src/pages/EmployeeLogin.jsx
+export default function EmployeeLogin() {
+    return (
+      <div className="min-h-screen flex items-center justify-center text-2xl">
+        Employee Login Page Coming Soon...
+      </div>
+    );
+  }
