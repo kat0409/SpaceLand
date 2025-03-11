7531875d81e0ea5c6aec5dfbe30096029eebcdbd
Markpelico <mapelico@cougarnet.uh.edu>
2025-03-11 16:41:21 -0500

    Added the Employee page for certain view

---
+++ Frontend/src/pages/UserPortal.jsx
@@ -0,0 +1,8 @@
+// src/pages/UserPortal.jsx
+export default function UserPortal() {
+    return (
+      <div className="min-h-screen flex items-center justify-center text-2xl">
+        User Portal Page Coming Soon...
+      </div>
+    );
+  }
