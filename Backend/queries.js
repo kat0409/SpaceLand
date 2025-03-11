7f8abc92d2685dd5ccf49738d62b8cf6984c55e8
kat0409 <kathiana119@gmail.com>
2025-03-10 23:47:27 -0500

    Made getSupervisors, getSupervisorIDbyDept, updateEmployeeForDeletion, and updateEmployeeForRehire functions
    Made getSupervisors, getSupervisorIDbyDept, updateEmployeeForDeletion, and updateEmployeeForRehire functions

--- Backend/queries.js
+++ Backend/queries.js
@@ -4,7 +4,7 @@ const getRides = 'SELECT * FROM rides';
 
 const getEmployees = 'SELECT * FROM employee';
 
-const addEmployee = 'INSERT INTO employee (FirstName, LastName, JobRole, Email, Address, SupervisorID, username, password) VALUES (?,?,?,?,?,?,?,?)';
+const addEmployee = 'INSERT INTO employee (FirstName, LastName, Email, Address, SupervisorID, username, password, Department, employmentStatus) VALUES (?,?,?,?,?,?,?,?,?)';
 
 const getRidesNeedingMaintenance = 'SELECT * FROM rides WHERE MaintenanceNeed = 1';
 
@@ -22,16 +22,41 @@ const getRestaurantTransactions = 'SELECT * FROM restaurantTransactions';
 
 const addRestaurantTransaction = 'INSERT INTO restaurantTransactions (restaurantID, VisitorID, transactionDate, Amount) VALUES (?, ?, ?, ?)';
 
+const getSupervisors = 'SELECT * FROM supervisors';
+
+const getSupervisorIDbyDept = 'SELECT SupervisorID FROM supervisors WHERE departmentName = ?';
+
+const updateEmployeeForDeletion = 'UPDATE employee SET employmentStatus = 0 WHERE EmployeeID = ?';
+
+const updateEmployeeForRehire = 'UPDATE employee SET employmentStatus = 1 WHERE EmployeeID = ?';
+
+
 //getRestaurantTransactions
 //getTickets
 //addTickets
 //getTicketTransactions
 //getVisitors
 //addVisitor
+//getAllVisitors
+//getOneVisitor
 //getDisabledVisitors
 //getWeather
 //getInclementWeather
 //getDaysClosedForWeather
 //addDayClosedForWeather
+//setEmployeeToRehire
+//setEmployeeToDelete
+//updateEmployeeInfo
+//authenticateUser
+//getSupervisorInfo
+//getEmployeeInfo
+//decodeToken
+//updateVisitorInfo
+//updateSupervisorInfo
+//updateAllEmployees
+//addMerchItem
+//getMerchItem
+//updateMerchItem
+//
 
 //Reports?
