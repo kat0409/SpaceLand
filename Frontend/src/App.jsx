0959352df0f7fbde37fe0b43edecb6c91e10939a
Markpelico <mapelico@cougarnet.uh.edu>
2025-03-11 16:08:27 -0500

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Hero from './components/Hero';
 import RidesPreview from './components/RidesPreview';
 import EventsPreview from './components/EventsPreview';
 import Footer from './components/Footer';
+import PricingSection from './components/PricingSection';
+import DiningSection from './components/DiningSection';
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
       <Hero />
       <RidesPreview />
       <EventsPreview />
+      <DiningSection />
+      <PricingSection />
       <Footer />
     </div>
   );
