de17e21dd83d5d6c6f34aedbb0c2eec20bbdf9d6
kat0409 <kathiana119@gmail.com>
2025-03-10 19:43:48 -0500

    Made getEmployees function
    Made function that queries the database to get all the tuples in the employee table.

--- Backend/actions.js
+++ Backend/actions.js
@@ -1,3 +1,4 @@
+const { stringify } = require('qs');
 const pool = require('./db.js');
 const queries = require('./queries.js');
 
@@ -10,7 +11,23 @@ const getRides = (request, response) => {
             response.end(JSON.stringify({error: "Internal server error"}));
             return;
         }
+        response.writeHead(200, {"Content-Type": "application/json"});
+        response.end(JSON.stringify(results));
     });
-    response.writeHead(200, {"Content-Type": "application/json"});
-    response.end(JSON.stringify(results));
 };
+
+//Fetch all employees
+const getEmployees = (request, response) => {
+    pool.query(queries.getEmployees, (error, results) => {
+        if (error){
+            console.error("Error fetching employees:", error);
+            response.writeHead(500, {"Content-Type": "application/json"});
+            response.end(JSON.stringify({error: "Internal server error"}));
+            return;
+        }
+        response.writeHead(200, {"Content-Type": "application/json"});
+        response.end(JSON.stringify(results));
+    });
+};
+
+//
