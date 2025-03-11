c3e1a4aa8f693bc1d8b50cac1a0d236f23671a18
Markpelico <mapelico@cougarnet.uh.edu>
2025-03-11 15:52:22 -0500

    initialize main.jsx and global styling
    created a footer header and hero file

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+      <header className="w-full bg-black text-white px-6 py-4">
+        <h1 className="text-2xl font-bold">Spaceland 🚀</h1>
+      </header>
+    );
+  }
