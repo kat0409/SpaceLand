a88802fcbcaf2674e8fd7179c3d9dd67df59b0f2
kat0409 <kathiana119@gmail.com>
2025-03-11 14:10:09 -0500

    created a middleware object to connect the front end and backend
    created a middleware object to connect the front end and backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,27 +1,25 @@
 const http = require('http');
-const {getRides, getEmployees, addEmployee, getRidesNeedingMaintenance, addMaintenance } = require('./functions.js');
+const url = require('url');
+const cors = require('cors');
+const eRoutes = require('./routes');
 
 //If you are calling a getter function, use GET
 //If you are calling an add function, use POST
 
-const server = http.createServer((request, response) => {
-    const parsedURL = new URL(request.url, `http://${request.headers.host}`);
+const corsMiddleWare = cors();
 
-    // Route handling
-    if (parsedUrl.pathname === '/rides' && req.method === 'GET') {
-        getRides(req, res);
-    } else if (parsedUrl.pathname === '/employees' && req.method === 'GET') {
-        getEmployees(req, res);
-    } else if (parsedUrl.pathname === '/add-employee' && req.method === 'POST') {
-        addEmployee(req, res);
-    } else if (parsedUrl.pathname === '/rides-needing-maintenance' && req.method === 'GET') {
-        getRidesNeedingMaintenance(req, res);
-    } else if (parsedUrl.pathname === '/add-maintenance' && req.method === 'POST') {
-        addMaintenance(req, res);
-    } else {
-        res.writeHead(404, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ error: "Route not found" }));
-    }
+//GET: fetch data from the database
+//POST: add data to the database
+//PUT: update data in the database
+
+const routes = {
+    'GET' : [
+
+    ]
+};
+
+const server = http.createServer((request, response) => {
+
 });
 
 const PORT  = process.env.PORT || 3000 //check if there is an environment variable
