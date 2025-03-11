d2151240e58ff5f0c1342328103d5022516b2da0
Markpelico <mapelico@cougarnet.uh.edu>
2025-03-11 16:39:50 -0500

    Create Rides.jsx

---
+++ Frontend/src/pages/Rides.jsx
@@ -0,0 +1,8 @@
+// src/pages/Rides.jsx
+export default function Rides() {
+    return (
+      <div className="min-h-screen flex items-center justify-center text-2xl">
+        Rides Page Coming Soon...
+      </div>
+    );
+  }
