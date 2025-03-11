45eb2342ec4a057f250d578bddd7b4b9903d00f7
kat0409 <kathiana119@gmail.com>
2025-03-10 19:01:46 -0500

    Made getRides function
    Sends an error (500) if there was an internal server error, or a success message (200) if all the rides could be retrieved from the database.

---
+++ Backend/actions.js
@@ -0,0 +1,16 @@
+const pool = require('./db.js');
+const queries = require('./queries.js');
+
+//Fetch all rides
+const getRides = (request, response) => {
+    pool.query(queries.getRides, (error, results) => {
+        if(error){
+            console.error("Error fetching rides:", error);
+            response.writeHead(500, {'Content-Type': "application/json"});
+            response.end(JSON.stringify({error: "Internal server error"}));
+            return;
+        }
+    });
+    response.writeHead(200, {"Content-Type": "application/json"});
+    response.end(JSON.stringify(results));
+};
