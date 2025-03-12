c0526b618132a15eaeba5931295ba2be51e2b244
celesteis-blue <c.cabrales708@gmail.com>
2025-03-12 18:57:13 -0500

    Update actions.js

--- Backend/actions.js
+++ Backend/actions.js
@@ -204,19 +204,6 @@ const getMerchandiseTransactions = (request, response) =>{
         response.end(JSON.stringify(results));
     });
 }
-const getEmployeesByDept = (req, res) => {
-    const { department } = req.query;
-    pool.query(queries.getEmployeesByDepartment, [department], (error, results) => {
-        if (error) {
-            console.error("Error fetching employees by department:", error);
-            res.writeHead(500, { "Content-Type": "application/json" });
-            res.end(JSON.stringify({ error: "Internal server error" }));
-            return;
-        }
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify(results));
-    });
-};
 
 //Check to see if you need to make a module.exports function here as well
 module.exports = {
