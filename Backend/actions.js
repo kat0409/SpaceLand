ac93bf804d59216b7e4a5cc17422ffd201416e7d
celesteis-blue <c.cabrales708@gmail.com>
2025-03-12 19:24:13 -0500

    Made getTicketSales function

--- Backend/actions.js
+++ Backend/actions.js
@@ -489,6 +489,18 @@ const getSalesReport = (req, res) => {
         res.end(JSON.stringify(results));
     });
 };
+const getTicketSales = (req, res) => {
+    pool.query(queries.getTicketSales, (error, results) => {
+        if (error) {
+            console.error("Error fetching ticket sales:", error);
+            res.writeHead(500, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ error: "Internal server error" }));
+            return;
+        }
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(JSON.stringify(results));
+    });
+};
 
 
 //Check to see if you need to make a module.exports function here as well
