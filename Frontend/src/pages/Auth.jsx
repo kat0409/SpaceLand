ae3979593e3ecd151319e680ea184c4f5c2f110a
kat0409 <kathiana119@gmail.com>
2025-03-13 19:20:36 -0500

    CONNECTED API ENDPOINT TO ALLOW USERS TO CREATE ACCOUNTS
    Co-Authored-By: Markpelico <183856734+Markpelico@users.noreply.github.com>

--- Frontend/src/pages/Auth.jsx
+++ Frontend/src/pages/Auth.jsx
@@ -54,7 +54,7 @@ export default function Auth() {
     };
 
     try {
-      const res = await fetch(`http://localhost:3000/`, {
+      const res = await fetch(`http://localhost:3000/add-visitor`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
