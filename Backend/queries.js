ea582161c49871418e0571f26816c9e17f4a3f5f
Markpelico <mapelico@cougarnet.uh.edu>
2025-03-15 13:15:46 -0500

    updated to kats backend
    Co-Authored-By: kskappad <165191350+kskappad@users.noreply.github.com>

--- Backend/queries.js
+++ Backend/queries.js
@@ -12,7 +12,7 @@ const addMaintenance = 'INSERT INTO maintenance (RideID, MaintenanceStartDate, M
 
 const getMerchandiseTransactions = 'SELECT * FROM merchandiseTransactions';
 
-const addMerchandiseTransaction = 'INSERT INTO merchandiseTransactions (VisitorID, transactionDate, quantity, totalAmount) VALUES (?,?,?,?)';
+const addMerchandiseTransaction = 'INSERT INTO merchandiseTransactions (merchandiseID, VisitorID, transactionDate, quantity, totalAmount) VALUES (?,?,?,?,?)';
 
 const addRestaurant = 'INSERT INTO restaurant (restaurantName, mealPlanTier) VALUES (?,?)';
 
@@ -56,6 +56,10 @@ const markNotificationAsSent = `
 `;
 
 const authenticateVisitor = 'SELECT * FROM visitors WHERE username = ? AND password = ?';
+
+const authenticateEmployee = 'SELECT * FROM employee WHERE username = ? AND password = ?';
+
+const authenticateSupervisor = 'SELECT * FROM supervisors WHERE username = ? AND password = ?';
 
 const addVisitor = `
     INSERT INTO visitors (FirstName, LastName, Phone, Email, Address, DateOfBirth, 
@@ -105,6 +109,105 @@ const getVisitorRecords = `
     SELECT * FROM visitors
 `;
 
+const addRide = `
+    INSERT INTO rides (RideID, RideName, MinHeight, MaxWeight,Capacity,Duration,MaintenanceStatus, MaintenanceNeed)
+    VALUES (?,?,?,?,?,?,?,?)
+`;
+
+//Reports
+const lowStockMerchandiseReport = `
+    SELECT 
+        m.itemName AS Merchandise_Item,
+        m.quantity AS Remaining_Stock,
+        s.email AS Supervisor_Email,
+        ln.timestamp AS Notification_Date
+    FROM 
+        lowstocknotifications ln
+    JOIN 
+        merchandise m ON ln.merchandiseID = m.merchandiseID
+    JOIN 
+        supervisors s ON ln.supervisorID = s.SupervisorID
+    WHERE 
+        ln.sent = FALSE;
+`;
+
+const rideMaintenanceReport = `
+    SELECT 
+        r.RideName AS Ride,
+        m.MaintenanceStartDate AS Start_Date,
+        m.MaintenanceEndDate AS End_Date,
+        CONCAT(e.FirstName, ' ', e.LastName) AS Maintenance_Employee
+    FROM 
+        maintenance m
+    JOIN 
+        rides r ON m.RideID = r.RideID
+    JOIN 
+        employee e ON m.MaintenanceEmployeeID = e.EmployeeID;
+`;
+
+const visitorPurchasesReport = `
+    SELECT 
+        v.VisitorID,
+        CONCAT(v.FirstName, ' ', v.LastName) AS Visitor_Name,
+        t.ticketType AS Ticket_Type,
+        tt.quantity AS Ticket_Quantity,
+        tt.totalAmount AS Ticket_Total_Spent,
+        m.itemName AS Merchandise_Bought,
+        mt.quantity AS Merchandise_Quantity,
+        mt.totalAmount AS Merchandise_Total_Spent
+    FROM 
+        visitors v
+    LEFT JOIN 
+        tickets t ON v.VisitorID = t.VisitorID
+    LEFT JOIN 
+        tickettransactions tt ON t.ticketID = tt.ticketID
+    LEFT JOIN 
+        merchandisetransactions mt ON v.VisitorID = mt.VisitorID
+    LEFT JOIN 
+        merchandise m ON mt.merchandiseID = m.merchandiseID;
+`;
+
+const attendanceAndRevenueReport = `
+    SELECT 
+        oh.date AS Operating_Date,
+        COUNT(t.ticketID) AS Tickets_Sold,
+        SUM(tt.totalAmount) AS Total_Ticket_Revenue,
+        oh.weatherCondition AS Weather_Condition
+    FROM 
+        operating_hours oh
+    JOIN 
+        tickets t ON oh.ticketID = t.ticketID
+    JOIN 
+        tickettransactions tt ON t.ticketID = tt.ticketID
+    GROUP BY 
+        oh.date, oh.weatherCondition
+    ORDER BY 
+        oh.date DESC;
+`;
+
+const getVisitorAccountInfo = `
+    SELECT v.FirstName, v.LastName, v.Phone,
+    v.Email, v.Address, v.DateOfBirth, v.AccessibilityNeeds,
+    v.Gender, v.Height, v.Age, v.MilitaryStatus
+    FROM visitors v
+    WHERE v.Username = ? AND v.Password = ?;
+`;
+
+const getEmployeeAccountInfo = `
+    SELECT e.FirstName, e.LastName, e.EmployeeID, e.Email,
+    e.Address, e.SupervisorID, e.Department,
+    e.employmentStatus, e.dateOfBirth
+    FROM employee e
+    WHERE e.username = ? AND e.password = ?;
+`;
+
+const getSupervisorAccountInfo = `
+    SELECT s.FirstName, s.LastName, s.SupervisorID, s.email,
+    s.address, s.departmentName,
+    s.gender, s.dateOfBirth, s.phoneNumber
+    FROM supervisors s
+    WHERE s.username = ? AND s.password = ?;
+`;
 
 module.exports = {
     getRides,
@@ -139,7 +242,17 @@ module.exports = {
     getTicketSales,
     getVisitorRecords,
     updateMaintenanceDates,
-    updateRideMaintenanceStatus
+    updateRideMaintenanceStatus,
+    authenticateEmployee,
+    lowStockMerchandiseReport,
+    rideMaintenanceReport,
+    visitorPurchasesReport,
+    attendanceAndRevenueReport,
+    getVisitorAccountInfo,
+    getEmployeeAccountInfo,
+    getSupervisorAccountInfo,
+    addRide,
+    authenticateSupervisor
 };
 
 //checkMerchQuantity
