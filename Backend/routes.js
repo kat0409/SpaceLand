8b9f44515f7096b4aeb0c5de5f6327b0bd4ab3b5
kat0409 <kathiana119@gmail.com>
2025-03-15 13:33:37 -0500

    Made functions to check if I ride exists and to add a ride to the database if it does not already exist in the database

--- Backend/routes.js
+++ Backend/routes.js
@@ -29,10 +29,6 @@ function routes(req, res) {
         return actions.getMerchandiseTransactions(req, res);
     }
 
-    if(url.startsWith('/add-merchandise-transaction') && method === 'POST'){
-        return actions.addMerchandiseTransaction(req,res);
-    }
-
     if (url.startsWith('/login') && method === 'POST') {
         return actions.loginVisitor(req, res);
     }
@@ -93,6 +89,10 @@ function routes(req, res) {
         return actions.rideMaintenanceReport(req,res);
     }
 
+    if(url.startsWith('/add-merchandise-transaction') && method === 'POST'){
+        return actions.addMerchandiseTransaction(req,res);
+    }
+
     if(url.startsWith('/supervisor/visitor-purchases') && method === 'GET'){
         return actions.visitorPurchasesReport(req,res);
     }
@@ -114,14 +114,14 @@ function routes(req, res) {
     }
 
     if(url.startsWith('/supervisor/add-ride') && method === 'POST'){//make
-        return actions.addMerchandiseTransaction(req,res);
+        return actions.addRide(req,res);
     }
 
-    if(url.startsWith('/supervisor/login') && method === 'POST'){//make
+    if(url.startsWith('/supervisor/login') && method === 'POST'){
         return actions.loginSupervisor(req,res);
     }
 
-    if(url.startsWith('/employee/login') && method === 'POST'){//make
+    if(url.startsWith('/employee/login') && method === 'POST'){
         return actions.loginEmployee(req,res);
     }
 
