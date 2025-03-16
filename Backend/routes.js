81ca15068ea01c7414335d02f58d20ec8edbd383
celesteis-blue <c.cabrales708@gmail.com>
2025-03-16 14:46:06 -0500

    Made route for deleteEmployee function
    Allows supervisors to delete employees, reused code from routes.js

--- Backend/routes.js
+++ Backend/routes.js
@@ -133,6 +133,10 @@ function routes(req, res) {
         return actions.updateMealPlan(req,res);
     }
 
+    if(url.startsWith('/supervisor/delete-employee') && method === 'PUT'){
+        return actions.updateMealPlan(req,res);
+    }
+
     res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ error: 'Route not handled by router' }));
 }
