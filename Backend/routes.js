85333a5abf4ad393f88f160490e69f848e630f36
celesteis-blue <c.cabrales708@gmail.com>
2025-03-16 00:09:46 -0500

    Added route for supervisor to update employee info

--- Backend/routes.js
+++ Backend/routes.js
@@ -129,6 +129,10 @@ function routes(req, res) {
         return actions.updateMealPlan(req,res);
     }
 
+    if(url.startsWith('/supervisor/update-employee-info') && method === 'PUT'){
+        return actions.updateMealPlan(req,res);
+    }
+
     res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ error: 'Route not handled by router' }));
 }
