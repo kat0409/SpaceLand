30feaa3457746c0694f76e0f11ea4a1b60f5050c
kat0409 <kathiana119@gmail.com>
2025-04-03 22:57:24 -0500

    Debugging

--- Backend/queries.js
+++ Backend/queries.js
@@ -187,7 +187,7 @@ const visitorPurchasesReport = `
         merchandisetransactions mt ON v.VisitorID = mt.VisitorID
     LEFT JOIN 
         merchandise m ON mt.merchandiseID = m.merchandiseID
-    WHERE 1=1; 
+    WHERE 1=1
 `;
 //check the WHERE 1=1 during debugging
 
