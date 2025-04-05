e94738eb27be48bd84fbc57ea3b6db212073c1b2
kat0409 <kathiana119@gmail.com>
2025-04-05 15:52:49 -0500

    Made routes to retrieve any pending orders to add them in a drop down menu inside the stock arrivals data entry form

--- Frontend/src/pages/StockArrival.jsx
+++ Frontend/src/pages/StockArrival.jsx
@@ -3,12 +3,43 @@ import { useState, useEffect } from 'react';
 export default function StockArrivalForm(){
     const [items, setItems] = useState([]);
     const [selectedItem, setSelectedItem] = useState('');
-    const [quantityAdded, setQuantityAdded] = useState('');
+    const [quantity, setQuantity] = useState('');
     const [arrivalDate, setArrivalDate] = useState('');
     const [notes, setNotes] = useState('');
     const [message, setMessage] = useState('');
 
     const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'https://spaceland.onrender.com';
 
+    useEffect(() => {
+        fetch(`${BACKEND_URL}/supervisor/merchandise/items`)
+        .then((res) => res.json())
+        .then((data) => {
+            if(Array.isArray(data)){ 
+                setItems(data);
+            }
+            else{
+                console.error("Unexpected response:", data);
+                setItems([]);
+            }
+        })
+        .catch((err) => {
+            console.error('Error fetching merchandise:', err);
+            setItems([]);
+        });
+    }, []);
 
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const res = await fetch(`${BACKEND_URL}/supervisor/merchandise/stock-arrivals`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            merchandiseID: selectedItem,
+            quantityAdded: quantity,
+            arrivalDate: arrivalDate,
+            reorderID: 
+            notes
+        }),
+    });
 };
