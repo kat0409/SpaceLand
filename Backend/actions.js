0a65fd70b376c387dd128f88eda1bcd74c9cbaec
celesteis-blue <c.cabrales708@gmail.com>
2025-04-05 14:43:04 -0500

    Added function to get merchandise employees
    Also added to module exports, reused code from actions.js modified to get merch employees
    
    Co-Authored-By: kat0409 <147408482+kat0409@users.noreply.github.com>

--- Backend/actions.js
+++ Backend/actions.js
@@ -1261,6 +1261,20 @@ const updateMealPlan = (req, res) => {
     };
 
 
+    const getMerchandiseEmployees = (res) => {
+        pool.query(queries.getMerchandiseEmployees, (error, results) => {
+            if (error){
+                console.error("Error fetching maintenance employees:", error);
+                res.writeHead(500, {"Content-Type": "application/json"});
+                res.end(JSON.stringify({error: "Internal server error"}));
+                return;
+            }
+            res.writeHead(200, {"Content-Type": "application/json"});
+            res.end(JSON.stringify(results));
+        });
+    };
+
+
 
 //Check to see if you need to make a module.exports function here as well
 module.exports = {
@@ -1299,6 +1313,7 @@ module.exports = {
     updateMealPlan,
     deleteEmployee,
     updateVisitorInfo,
-    getMaintenanceEmployees
+    getMaintenanceEmployees,
+    getMerchandiseEmployees
 
 };
