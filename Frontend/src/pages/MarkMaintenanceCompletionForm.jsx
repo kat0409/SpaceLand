cd81a499b3aa5fd2dbe91f24f0d01b0f9a4af17f
kat0409 <kathiana119@gmail.com>
2025-04-07 02:24:36 -0500

    Made query to fetch pending maintenance requests and made some of the data entry form front end

---
+++ Frontend/src/pages/MarkMaintenanceCompletionForm.jsx
@@ -0,0 +1,18 @@
+import {useState, useEffect} from "react";
+
+export default function MarkMaintenanceCompletionForm(){
+    const [requests, setRequests] = useState([]);
+    const [selectedRequest, setSelectedRequest] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [message, setMessage] = useState('');
+
+    const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'https://spaceland.onrender.com';
+
+    useEffect(() => {
+        fetch(`${BACKEND_URL}/supervisor/maintenance/ridemaintenance-pending`)
+            .then(res => res.json())
+            .then(data => setRequests(data))
+            .catch(err => console.error("Error fetching pending maintenance requests:", err));
+    }, []);
+
+}
