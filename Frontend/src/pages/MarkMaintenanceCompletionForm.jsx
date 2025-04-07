9579f828d4a85624b2fadb1f5539d04fa393ffbf
kat0409 <kathiana119@gmail.com>
2025-04-07 02:39:05 -0500

    Made routes to fetch pending maintenance requests

--- Frontend/src/pages/MarkMaintenanceCompletionForm.jsx
+++ Frontend/src/pages/MarkMaintenanceCompletionForm.jsx
@@ -14,5 +14,6 @@ export default function MarkMaintenanceCompletionForm(){
             .then(data => setRequests(data))
             .catch(err => console.error("Error fetching pending maintenance requests:", err));
     }, []);
-
+    
+    const
 }
