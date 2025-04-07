3fbbfd9ec1d441918c8b2d81ca4c20d7c7f75eb5
kat0409 <kathiana119@gmail.com>
2025-04-07 13:31:42 -0500

    Made routes to get the purchases from a specific visitor

--- Backend/routes.js
+++ Backend/routes.js
@@ -195,6 +195,10 @@ function routes(req, res) {
         return actions.getMaintenanceRequests(req,res);
     }
 
+    if(url.startsWith('/purchase-history') && method === "GET"){
+        return actions.getVisitorMerchPurchases(req,res);
+    }
+
     res.writeHead(404, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ error: 'Route not handled by router' }));
 }
