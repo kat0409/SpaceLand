aaafc8eab3f11e5a5ef656a0c025dc933fa44443
Markpelico <mapelico@cougarnet.uh.edu>
2025-04-14 20:04:06 -0500

    deleted fileupload.js

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -18,7 +18,6 @@ import MerchandiseSupervisorPortal from './pages/MerchandiseSupervisorPortal';
 import MaintenanceSupervisorPortal from './pages/MaintenanceSupervisorPortal';
 import HRSupervisorPortal from './pages/HRSupervisorPortal';
 import Shopping from './pages/Shopping';
-import PaymentForm from './pages/PaymentForm';
 
 export default function App() {
   return (
@@ -47,7 +46,6 @@ export default function App() {
           <Route path="/supervisor/merchandise" element={<MerchandiseSupervisorPortal />} />
           <Route path="/supervisor/maintenance" element={<MaintenanceSupervisorPortal />} />
           <Route path="/supervisor/HR" element={<HRSupervisorPortal />} />
-          <Route path="/payment" element={<PaymentForm />} />
         </Routes>
       </motion.div>
     </Router>
